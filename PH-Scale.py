48c3e3facf79c13c57829688eb14602d78d8b76b
Abdul Moeez <91822215+mianabdulmoez@users.noreply.github.com>
2023-08-13 12:56:03 +0500

    Add files via upload

---
+++ PH-Scale.py
@@ -0,0 +1,11 @@
+ph = int(input("What is PH of your chemical(0-14): "))
+if ph > 8:
+    print("It is a BASIC")
+elif ph < 6:
+    print("It is a ACIDIC")
+else:
+    print("It is NEUTRAL")
+
+
+
+    
